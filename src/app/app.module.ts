dcbc51823598f883298281439db6a3892dcdc097
daibo <bobriandai@gmail.com>
2019-07-19 12:41:39 -0400

    add readme file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { TooltipModule } from './home/tooltip/tooltip.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
